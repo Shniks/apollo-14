1905c40543be0c3a4b3e0562cb35747263a5905a
Nikhil Shahi <shniks@gmail.com>
2022-08-11 11:40:50 -0700

    Created model test for mission model for the alphabetical method. Completed user story 3.

--- spec/models/mission_spec.rb
+++ spec/models/mission_spec.rb
@@ -10,4 +10,16 @@ describe Mission, type: :model do
     it { should have_many :astronaut_missions}
     it { should have_many :astronauts}
   end
+
+  it 'can sort missions alphabetically' do
+    mission_1 = Mission.create!(title: 'Apollo 13', time_in_space: 400)
+    mission_2 = Mission.create!(title: 'Gemini 7', time_in_space: 400)
+    mission_3 = Mission.create!(title: 'Capricon 4', time_in_space: 400)
+
+    sorted = Mission.alphabetical
+
+    expect(sorted.first.title).to eq(mission_1.title)
+    expect(sorted[1].title).to eq(mission_3.title)
+    expect(sorted.last.title).to eq(mission_2.title)
+  end
 end
