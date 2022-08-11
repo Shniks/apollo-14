d9af7fe7b334d0f9f84cedf91928a6422ef0f896
Nikhil Shahi <shniks@gmail.com>
2022-08-11 12:54:13 -0700

    Created feature test for user to log in to the app.

---
+++ spec/features/user_can_log_in_into_app_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'When I visit the root path, I see a link to login' do
+  describe 'When I click link and fill in log in details' do
+    it 'I get logged into the app' do
+      user = User.create(username: "funbucket13", password: "test")
+
+      visit '/'
+
+      click_on 'I already have an account'
+
+      expect(current_path).to eq('/login')
+
+      fill_in :username, with: user.username
+      fill_in :password, with: user.password
+
+      click_on 'Log In'
+
+      expect(current_path).to eq('/')
+      expect(page).to have_content("Welcome, #{username}!")
+      expect(page).to_not have_content("Register as a User")
+      expect(page).to_not have_content("I already have an account")
+    end
+  end
+end
