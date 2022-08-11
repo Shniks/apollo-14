42508d6c9abd0833e471b32a864f2644613d78de
Nikhil Shahi <shniks@gmail.com>
2022-08-11 12:34:43 -0700

    Created and ran migration to add a users table.

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true
+  validates_presence_of :password, require: true
+
+  has_secure_password
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_04_152506) do
+ActiveRecord::Schema.define(version: 2022_08_11_191843) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(version: 2018_09_04_152506) do
   create_table "missions", force: :cascade do |t|
     t.string "title"
     t.integer "time_in_space"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "astronaut_missions", "astronauts"
